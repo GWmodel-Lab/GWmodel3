0246f1dbb97c41f7ecf8cb5c8e1e1a182d49d732
Yigong Hu <huyg0180110559@outlook.com>
2023-10-31 10:35:03 +0000

    Feature: specific MGWR configuration by name (#12)
    * add: allow specific config by names
    
    * add: docs for config specification by names
    
    * edit: update docs [skip ci]
    
    * edit: specified bw value in vignette

--- tests/testthat/test-gwr_multiscale.R
+++ tests/testthat/test-gwr_multiscale.R
@@ -8,6 +8,43 @@ test_that("Multiscale GWR: works", {
   ))
 })
 
+test_that("Multiscale GWR: specific config by names", {
+  #### all by name
+  expect_no_error({
+    gwr_multiscale(
+      formula = PURCHASE ~ FLOORSZ + UNEMPLOY + PROF,
+      data = LondonHP,
+      config = list(
+        Intercept = mgwr_config(adaptive = TRUE),
+        FLOORSZ = mgwr_config(adaptive = TRUE),
+        UNEMPLOY = mgwr_config(adaptive = TRUE),
+        PROF = mgwr_config(adaptive = TRUE)
+      )
+    )
+  })
+  #### use default value
+  expect_no_error({
+    gwr_multiscale(
+      formula = PURCHASE ~ FLOORSZ + UNEMPLOY + PROF,
+      data = LondonHP,
+      config = list(
+        FLOORSZ = mgwr_config(bw = 30, optim_bw = "no", adaptive = TRUE),
+        .default = mgwr_config(adaptive = TRUE)
+      )
+    )
+  })
+  #### error when default is missing and config is missing
+  expect_error({
+    gwr_multiscale(
+      formula = PURCHASE ~ FLOORSZ + UNEMPLOY + PROF,
+      data = LondonHP,
+      config = list(
+        FLOORSZ = mgwr_config(bw = 30, optim_bw = "no", adaptive = TRUE)
+      )
+    )
+  }, "Either specific configs for all variables, or provide a default config naming '.default'!")
+})
+
 test_that("Multiscale GWR: verbose", {
   skip_on_ci()
   expect_no_error(gwr_multiscale(
