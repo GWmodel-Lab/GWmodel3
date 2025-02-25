73ef22d6e5213a06185ed47539508a48b977e706
HPDell <huyg0180110559@outlook.com>
2025-02-25 19:32:17 +0000

    edit: remove call data from GWmodel3

--- tests/testthat/test-gwr_basic.R
+++ tests/testthat/test-gwr_basic.R
@@ -73,9 +73,4 @@ test_that("Basic GWR: CUDA for Windows development", {
     m_cuda <- gwr_basic(PURCHASE~FLOORSZ+UNEMPLOY, LondonHP, 64, TRUE, parallel_method = "cuda", parallel_arg = c(0, 64), verbose = 1)
     predict(m_cuda, LondonHP, verbose = 1)
   })
-  expect_no_error({
-    data(wuhan.hp, package = "hgwrr")
-    whhp <- with(wuhan.hp, sf::st_as_sf(data, coords = c("lon", "lat")))
-    gwr_basic(Price ~ d.PrimarySchool + BuildingArea + Fee, whhp, bw = 64, parallel_method = "cuda", parallel_arg = c(0, 64))
-  })
 })
